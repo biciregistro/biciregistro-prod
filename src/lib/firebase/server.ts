e06ce36ecbed7288320ffe1357e57c2503663a85
Hola Biciregistro <hola@biciregistro.mx>
2025-11-02 17:02:16 +0000

    Try fixing this error: `Runtime Error: SyntaxError: "undefined" is not v

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApp, getApps, App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
+import 'dotenv/config';
 
 const createFirebaseAdminApp = (): App => {
     if (getApps().length > 0) {
